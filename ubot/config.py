9dca1bc4a314141548fa57dacace6b9f33bd54a6
arthur130999 <165489760+arthur130999@users.noreply.github.com>
2024-08-29 10:13:07 +0700

    FIX

--- ubot/config.py
+++ ubot/config.py
@@ -27,31 +27,59 @@ DEVS = [
     5272203652,
 ]
 
-KYNAN = list(map(int, os.getenv("KYNAN", "1054295664 5272203652 5013987239 5357942628 1898065191").split()))
+<<<<<<< HEAD
+KYNAN = list(map(int, os.getenv("ARTHUR", "").split()))
+=======
+KYNAN = list(map(int, os.getenv("ARTHUR", "1054295664 5272203652 5013987239 5357942628 1898065191").split()))
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
+<<<<<<< HEAD
+API_ID = int(os.getenv("API_ID", ""))
+=======
 API_ID = int(os.getenv("API_ID", "22527580"))
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
+<<<<<<< HEAD
+API_HASH = os.getenv("API_HASH", "")
+=======
 API_HASH = os.getenv("API_HASH", "2ca384a5fdf3c2ed1b7702869a2db439")
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6742667446:AAFhjpdI7m6Pk1MZz0fS6qP9LFb0idcTQkE")
 
 SESSION = os.getenv("SESSION", "")
 
+<<<<<<< HEAD
+OWNER_ID = int(os.getenv("OWNER_ID", ""))
+=======
 OWNER_ID = int(os.getenv("OWNER_ID", "5272203652"))
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
+<<<<<<< HEAD
+USER_ID = list(map(int, os.getenv("USER_ID", "").split()))
+=======
 USER_ID = list(map(int, os.getenv("USER_ID", "1054295664 5272203652 5013987239 1898065191 5357942628 816526222 6104161118").split()))
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
+<<<<<<< HEAD
+LOG_UBOT = int(os.getenv("LOG_UBOT", ""))
+=======
 LOG_UBOT = int(os.getenv("LOG_UBOT", "-1002063271971"))
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002063271971 -1001608847572 -1001538826310 -1001876092598 -1001864253073 -1001451642443 -1001825363971 -1001797285258 -1001927904459 -1001287188817 -1001812143750 -1001608701614 -1001473548283").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
-RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+RMBG_API = os.getenv("RMBG_API", "")
 
 OPENAI_KEY = os.getenv(
     "OPENAI_KEY",
+<<<<<<< HEAD
+    "",
+=======
     "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",
+>>>>>>> dd496e293f887df8d2dbb65d6bec4d4f91f6dee3
 ).split()
 
 MONGO_URL = os.getenv(
